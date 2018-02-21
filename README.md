46a55a5970fcbf23bd2f06ae9c00929688a92e42
unknown <blkorneg@hotmail.com>
2018-02-20 21:05:00 -0500

    updates to liri

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # liri-node-app
+
+ LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+Commands for Liri is as follows
+
+*  node liri.jsmyTweets my-tweets
+
+*  node liri.js spotifyThisSong '<song name here>'
+
+*  node liri.js movieThis '<movie name here>'
+
+*  node liri.js doWhatItSays`
--- liri.js
+++ liri.js
@@ -1,1 +1,89 @@
-require("dotenv").config();
+//Import required JS,APIs,Config files etc
+var dotenv    = require("dotenv").config();
+var keys      = require("./key.js");
+var Twitter   = require("twitter");
+var Spotify   = require('node-spotify-api');
+var Request   = require("request");
+var fs        = require('fs');
+
+//Initalize twitter and spotify with respective keys
+var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
+
+//Capture Arguments from command line
+var command = process.argv[2];
+var query = process.argv[3] ? process.argv[3] : "big_time29";
+
+//using command pattern
+var liriManager = {
+  // request myTweets info
+    myTweets: function(screenName) {
+        var params = {screen_name: screenName};
+        client.get('statuses/user_timeline', params, function(error, tweets, response) {
+          if (!error) {
+            for (var i=0; i < tweets.length; i++) {
+            console.log(tweets[i].text);
+          }
+        }
+      });
+    },
+
+    // request song info
+    spotifyThisSong: function(songName){;
+      spotify.search({ type: 'track', query: songName }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        } else {
+                var trackList = data.tracks.items;
+                //Not sure why I can append Preview without creating var
+                var preview = trackList[0].preview_url ? trackList[0].preview_url : "Preview not available";
+                console.log("Title: " + trackList[0].name + "\nAlbum: " + trackList[0].album.name + "\nArtist: " + "\nPreview: " + preview);
+        }
+
+      });
+    },
+
+    //request movie info
+    movieThis: function(movieName) {
+      if (movieName === 'big_time29'){
+          console.log ("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/ " +
+          "\nIt's on Netflix");
+      } else {
+      // Then run a request to the OMDB API with the movie specified
+        Request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+            // If the request is successful (i.e. if the response status code is 200)
+            if (!error && response.statusCode === 200) {
+
+              // Parse the body getting items per requirments
+              console.log("Title: " + JSON.parse(body).Title + "\nYear: " + JSON.parse(body).Year + "\nIMDB Rating: " + JSON.parse(body).imdbRating +
+              "\nRotten Tomatoes: " + JSON.parse(body).Ratings[1].Value + "\nCountry: " + JSON.parse(body).Country + "\nLanguage: " +
+              JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors : " + JSON.parse(body).Actors);
+            }
+        });
+      }
+    },
+
+    // request doWhatItSays info
+    doWhatItSays: function(doCommand) {
+      fs.readFile('random.txt', (error, data) => {
+    		if (error) {
+    			throw error
+    		};
+
+  		  // Data is an buffer. Conver to string and split on the comma to create an array
+  		  var randomTxt = data.toString().split(',');
+  		  command = randomTxt[0];
+  		  query = randomTxt[1];
+
+        liriManager.execute(command,query);
+
+      });
+    }
+  };
+
+  // abstracting the API for decoupling
+  liriManager.execute = function ( name ) {
+      return liriManager[name] && liriManager[name].apply(liriManager, [].slice.call(arguments, 1) );
+  };
+
+  liriManager.execute(command,query);
